3d1bfdf8e16b59907b8f4b12036707637ae81aaa
Pranay Mahendrakar <mahendrakarpranay@gmail.com>
2026-01-06 11:18:32 +0530

    Initial commit

---
+++ 2025-12-27 20.55.05 - Accepted - runtime 51ms - memory 53.4MB.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    public:
+        int subarraysWithKDistinct(vector<int>& nums, int k) {
+                return atMost(nums, k) - atMost(nums, k - 1);
+                    }
+                        
+                            int atMost(vector<int>& nums, int k) {
+                                    if (k == 0) return 0;
+                                            unordered_map<int, int> count;
+                                                    int left = 0, res = 0;
+                                                            
+                                                                    for (int right = 0; right < nums.size(); right++) {
+                                                                                if (count[nums[right]] == 0) k--;
+                                                                                            count[nums[right]]++;
+                                                                                                        
+                                                                                                                    while (k < 0) {
+                                                                                                                                    count[nums[left]]--;
+                                                                                                                                                    if (count[nums[left]] == 0) k++;
+                                                                                                                                                                    left++;
+                                                                                                                                                                                }
+                                                                                                                                                                                            res += right - left + 1;
+                                                                                                                                                                                                    }
+                                                                                                                                                                                                            return res;
+                                                                                                                                                                                                                }
+                                                                                                                                                                                                                };
